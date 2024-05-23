8287a599ee6adfed203c41df207f64f5eec9358f
mgcooper <mguycooper@gmail.com>
2024-05-22 21:46:20 -0700

    style: Update descriptive variable names, apply linting.

--- code/uniform_time.m
+++ code/uniform_time.m
@@ -1,98 +1,100 @@
-function [tt1,y1] = uniform_time(tt0,y0,trange)
-% uniform time for SSA
-% {{SSA; time; uniform; LLZ; regulate}}
-PAR = struct('average',1,'interp',0,'gap',1/12);
-% PAR = var_initial(PAR0,varargin);
-% average=0, only search the nearest within gap/2
-% average=0, average the near month within gpa/2
-% interp=1, interpolate the missing months
+function [t_uniform, s_uniform] = uniform_time(t0, s0, trange)
+   % uniform time for SSA
+   
+   % {{SSA; time; uniform; LLZ; regulate}}
+   
+   PAR = struct('average',1,'interp',0,'gap',1/12);
+   % PAR = var_initial(PAR0,varargin);
+   % average=0, only search the nearest within gap/2
+   % average=0, average the near month within gpa/2
+   % interp=1, interpolate the missing months
 
-if nargin < 3
-    trange = [2002,4,2019,10];
+   if nargin < 3
+      trange = [2002,4,2019,10];
+   end
+
+   t_uniform = generate_tt(trange(1:2), trange(3:4));
+   s_uniform = nan(size(t_uniform));
+
+   for n = 1:numel(s_uniform)
+      dd = t0 - t_uniform(n);
+
+      if PAR.average == 0
+         [mindd, loc] = min(abs(dd));
+         if mindd <= PAR.gap/2
+            s_uniform(n) = s0(loc);
+         end
+      else
+         ind = dd<PAR.gap/2 & dd>=-PAR.gap/2;
+         s_uniform(n) = mean(s0(ind));
+      end
+   end
+
+   if PAR.interp == 1 % linear
+      ind = ~isnan(s_uniform);
+      s_uniform = interp1(t_uniform(ind), s_uniform(ind), t_uniform);
+   elseif PAR.interp == 2 % SSA
+      [~, RC, htest] = ssa_missing_GO(s_uniform, 48, 10);
+      s_uniform = sum(RC(:,htest==1),2);
+   end
+
 end
 
-tt1 = generate_tt(trange(1:2),trange(3:4));
-y1 = nan(size(tt1));
-for ii = 1:numel(y1)
-    dd = tt0 - tt1(ii);
-    if PAR.average == 0
-        [mindd,loc] = min(abs(dd));
-        if mindd <= PAR.gap/2
-            y1(ii) = y0(loc);
-        end
-    else
-        ind = dd<PAR.gap/2 & dd>=-PAR.gap/2;
-        y1(ii) = mean(y0(ind));
-    end
-end
+function [tt, sfilename] = generate_tt(t1,o_t2,idatenum)
+   % {{time}} {{create}} {{str}}
+   % t1 : start time, [year,month]
+   % t2 : end time
+   % input is ([y1,m1], [y2,m2]) or ([y1,m1, y2,m2]);
 
-if PAR.interp == 1 % linear
-    ind = ~isnan(y1);
-    y1 = interp1(tt1(ind),y1(ind),tt1);
-elseif PAR.interp == 2 % SSA
-    [~, RC, htest] = ssa_missing_GO(y1, 48, 10);
-    y1 = sum(RC(:,htest==1),2);
-end
+   if nargin == 1
+      if numel(t1) == 4
+         o_t2 = t1(3:4);
+      else
+         o_t2 = t1;
+      end
+   end
 
-end
+   if nargin < 3
+      idatenum = 0;
+   end
 
-function [tt,sfilename] = generate_tt(t1,o_t2,idatenum)
-% {{time}} {{create}} {{str}}
-% t1 : start time, [year,month]
-% t2 : end time
-% input is ([y1,m1], [y2,m2]) or ([y1,m1, y2,m2]);
-
-if nargin == 1
-    if numel(t1) == 4
-        o_t2 = t1(3:4);
-    else
-        o_t2 = t1;
-    end
-end
-
-if nargin < 3
-    idatenum = 0;
-end
-
-if numel(t1) == 1 
-    t1 = time_transfer(t1,3);
-    o_t2 = time_transfer(o_t2,3);
-end
+   if numel(t1) == 1
+      t1 = time_transfer(t1,3);
+      o_t2 = time_transfer(o_t2,3);
+   end
 
 
-iyear1 = t1(1);
-iyear2 = o_t2(1);
-imonth1 = t1(2);
-imonth2 = o_t2(2);
+   iyear1 = t1(1);
+   iyear2 = o_t2(1);
+   imonth1 = t1(2);
+   imonth2 = o_t2(2);
 
-% Ntt = (iyear2-iyear1)*12 - imonth1 + imonth2 +1;
-NN = (iyear2-iyear1+1)*12;
-rtmp(1:NN,1) = 0;
+   % Ntt = (iyear2-iyear1)*12 - imonth1 + imonth2 +1;
+   NN = (iyear2-iyear1+1)*12;
+   rtmp(1:NN,1) = 0;
 
-ik = 0;
-for iyear = iyear1:iyear2
-    for imonth = 1:12
-        ik = ik+1;
-        rtmp(ik) = (imonth-0.5)/12 + iyear;% - iyear1;
-    end
-end
+   ik = 0;
+   for iyear = iyear1:iyear2
+      for imonth = 1:12
+         ik = ik+1;
+         rtmp(ik) = (imonth-0.5)/12 + iyear;% - iyear1;
+      end
+   end
 
-tt = rtmp(imonth1:end-12+imonth2);
+   tt = rtmp(imonth1:end-12+imonth2);
 
-if idatenum == 1
-    tt = time_transfer(tt(:),-1);
-end
+   if idatenum == 1
+      tt = time_transfer(tt(:),-1);
+   end
 
-% ============================================================
-ik = 0;
-for iyear = iyear1:iyear2
-    for imonth = 1:12
-        ik = ik+1;
-        stmp{ik} = sprintf('%6.6d',iyear*100 + imonth);
-    end
-end
-% stmp{1:2}
-sfilename = stmp(imonth1:end-12+imonth2);
-
-
+   % ============================================================
+   ik = 0;
+   for iyear = iyear1:iyear2
+      for imonth = 1:12
+         ik = ik+1;
+         stmp{ik} = sprintf('%6.6d',iyear*100 + imonth);
+      end
+   end
+   % stmp{1:2}
+   sfilename = stmp(imonth1:end-12+imonth2);
 end
--- runGraceGapFill.m
+++ runGraceGapFill.m
@@ -1,13 +1,21 @@
-% clean % alias for clear all; close all; clc;
+clearvars
+close all
+clc
 
-saveData    = true;
-gapFill     = true;
-plotFigs    = true;    % if false, figures are not made
-showFigs    = true;     % if false, figure is made but not shown
-saveFigs    = false;
+saveDataFlag = true;
+runGapFillFlag = true;
+plotFiguresFlag = true; % if false, figures are not made
+showFiguresFlag = true; % if false, figure is made but not shown
+saveFiguresFlag = false;
 
-pathGrace   = '/path/to/GRACE/data/';
-pathSave    = '/path/to/save/the/data/';
+pathname = getenv('GRACE_DATA_PATH');
+filename = getenv('GRACE_DATA_FILE');
+
+% For plotting
+coastlat = load('coastlines.mat').('coastlat');
+coastlon = load('coastlines.mat').('coastlon');
+
+%% Data notes
 
 % the grace data used here is from the university of texas at austin:
 % http://www2.csr.utexas.edu/grace/RL06_mascons.html
@@ -19,161 +27,129 @@ pathSave    = '/path/to/save/the/data/';
 % the calendar is gregorian, which should be correct using the datetime
 % function method i use below, but not sure about time zone
 
-load('coastlines.mat'); % useful for plotting
+%% Read the GRACE data
+fileinfo = ncinfo(fullfile(pathname, filename));
+lat = ncread(fullfile(pathname, filename), 'lat');
+lon = ncread(fullfile(pathname, filename), 'lon');
+lwe = ncread(fullfile(pathname, filename), 'lwe_thickness');
+time = ncread(fullfile(pathname, filename), 'time');
 
-%~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-% read the GRACE data
-%~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+% time is 'days since 2002-01-01T00:00:00Z'. Convert to datetime.
+time = datetime(2002,1,1,0,0,0) + days(time);
 
-% read the grace data:
-fgrace      = 'CSR_GRACE_GRACE-FO_RL06_Mascons_all-corrections_v02.nc';
-finfo       = ncinfo([pathGrace fgrace]);
-Grace       = ncreaddata([pathGrace fgrace],{finfo.Variables.Name});
+%% Make calendars
+[Time_notFilled, Time_referencePeriod] = makeGraceCalendar(time, ...
+   "calendarType", "notfilled");
 
-% georeference the data
-[LAT,LON]   = meshgrat(double(Grace.lat),double(Grace.lon));
-LWE         = flipud(double(Grace.lwe_thickness));
-cellRes     = 1/4; % 1/4th degree
-latlim      = [min(LAT(:))-cellRes/2 max(LAT(:))+cellRes/2];
-lonlim      = [min(LON(:))-cellRes/2 max(LON(:))+cellRes/2];
-rasterSize  = size(LWE(:,:,1));
-R           = georefcells(latlim,lonlim,rasterSize,'ColumnsStartFrom','north');
+%% Georeference the data
+[LON, LAT] = meshgrid(double(lon), double(lat));
+LAT = flipud(LAT);
+LWE = flipud(permute(double(lwe), [2 1 3]));
+cellRes = 1/4; % 1/4th degree
+latlim = [min(LAT(:))-cellRes/2 max(LAT(:))+cellRes/2];
+lonlim = [min(LON(:))-cellRes/2 max(LON(:))+cellRes/2];
+rasterSize = size(LWE(:, :, 1));
+R = georefcells(latlim, lonlim, rasterSize, 'ColumnsStartFrom', 'north');
 
-% time is 'days since 2002-01-01T00:00:00Z'
-T1          = datetime(2002,1,1,0,0,0);
-T           = datetime(datenum(T1)+Grace.time,'ConvertFrom','datenum');
+%% Plot the data
+figure(1);
+hold on
+worldmap('world')
+geoshow(mean(LWE, 3), R, 'DisplayType', 'texturemap');
+plotm(coastlat, coastlon, 'k')
+colormap('parula')
+colorbar southoutside
+set(gca, 'ColorScale', 'log')
 
-% for reference, this would also work
-% T           = datetime(2002,1,1,0,0,0) + days(grace.time);
+%% Subset the data to a bounding box
 
-% build a bounding box around the arctic
-minlat      = 40; 
-minlon		= -170;
-maxlat      = 85; 
-maxlon 		= -48;
-bbox        = [minlon,minlat;maxlon,maxlat];
-xbox        = [bbox(1),bbox(2),bbox(2),bbox(1),bbox(1)];
-ybox        = [bbox(3),bbox(3),bbox(4),bbox(4),bbox(3)];
+% Build a bounding box around Alaska
+minlat = 40;
+minlon = -170;
+maxlat = 85;
+maxlon = -48;
+bbox = [minlon,minlat;maxlon,maxlat];
+xbox = [bbox(1),bbox(2),bbox(2),bbox(1),bbox(1)];
+ybox = [bbox(3),bbox(3),bbox(4),bbox(4),bbox(3)];
 
-% for plotting, plot the average 
-LWEavg      = mean(LWE,3);
-LWE0        = zeros(size(LWEavg));
-
-% crop the grace data to the study area 
-[LWEcrop,Rcrop] = geocrop(LWE,R,[minlat maxlat],[minlon maxlon]);
-LWEcrop0        = zeros(size(LWEcrop,1),size(LWEcrop,2));
+% Crop the grace data to a study area
+[LWEcrop, Rcrop] = geocrop(LWE, R, [minlat maxlat], [minlon maxlon]);
 
 % use the cropped data (LWE) and lat/lon
-LWE         = LWEcrop;
-R           = Rcrop;
-[LON,LAT]   = R2grat(R);
-LON         = wrapTo180(LON);       % the basins are -180:180 so wrap grace too
+LWE = LWEcrop;
+R = Rcrop;
+[LON, LAT] = R2grat(R);
+LON = wrapTo180(LON); % wrap grace coordinates to -180:180
 
-%~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-% plot it
-%~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+%% Plot the cropped data
+figure(2);
+hold on
+worldmap('World')
+geoshow(mean(LWEcrop,3), Rcrop, 'DisplayType', 'texturemap');
+plotm(coastlat, coastlon, 'k')
+colorbar
+set(gca, 'ColorScale', 'log')
 
-% plot the global data
-figure(1); 
-geoshow(LWE0,R,'CData',LWEavg,'DisplayType','texturemap');
-hold on; geoshow(coastlat,coastlon); colormap('parula'); colorbar
-set(gca,'ColorScale','log'); axis tight; 
-plot(xbox,ybox,'r')
+%% Plot with the wrapped Lon to make sure it worked
+figure(3)
+hold on
+worldmap('World')
+geoshow(LAT, LON, mean(LWE,3), 'DisplayType', 'texturemap');
+plotm(coastlat, coastlon, 'k')
+colorbar
+set(gca, 'ColorScale', 'log')
 
 
-% plot the cropped data
-figure(2); 
-geoshow(LWEcrop0,Rcrop,'CData',mean(LWEcrop,3),'DisplayType','texturemap'); 
-hold on; geoshow(coastlat,coastlon); colorbar
-set(gca,'ColorScale','log'); axis tight; % plotBbox(bbox,'r');
-plot(xbox,ybox,'r')
 
-
-% plot with the wrapped Lon to make sure it worked
-figure(3); geoshow(LAT,LON,mean(LWE,3),'DisplayType','texturemap'); 
-hold on; geoshow(coastlat,coastlon); colorbar
-set(gca,'ColorScale','log'); axis tight; plot(xbox,ybox,'r');
-
-%~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-% Gap filling
-%~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-% make a calendar for the gap-filled data, for the reference period
-TnoFill = T;
-idx     = isbetween(TnoFill,datetime(2004,1,1),datetime(2009,12,31));
-Tref    = TnoFill(idx);
-Tfill   = datetime(TnoFill(1):calmonths(1):TnoFill(end)+3);
-
-% reset the indices to be relative to the grace data
-nmonths = numel(Tfill);
-Tgrace  = Tfill;
-idx     = isbetween(Tgrace,datetime(2004,1,1),datetime(2009,12,31));
-
+%% Gap filling
 
 % reshape the LWE and lat/lon to find points in each polygon
-lwe     = reshape(LWE,size(LWE,1)*size(LWE,2),size(LWE,3));
-lon     = reshape(LON,size(LON,1)*size(LON,2),1);
-lat     = reshape(LAT,size(LAT,1)*size(LAT,2),1);
-buffer  = 0;
+lwe = reshape(LWE, size(LWE,1) * size(LWE,2), size(LWE,3));
+lon = reshape(LON, size(LON,1) * size(LON,2), 1);
+lat = reshape(LAT, size(LAT,1) * size(LAT,2), 1);
+buffer = 0;
 
-% % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % 
+%% Example of running for one point
 
-% example of running for one point
-
-% % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % 
-
-% send these options to the function
-opts.showFigs   = showFigs;
-opts.plotFigs   = plotFigs;
-opts.saveFigs   = saveFigs;
-opts.pathSave   = pathSave;
-
-% note - if showFigs is true, the function below will make a figure for
+% If showFiguresFlag is true, the function below will make a figure for
 % every timeseries and pause to look at it, so you have to press any button
-% to advance to the next point. If you set plotFigs true but showFigs
-% false, and saveFigs true, the figures will be made and saved to look at
-% later
+% to advance to the next point. If you set plotFiguresFlag true but
+% showFiguresFlag false, and saveFiguresFlag true, the figures will be made 
+% and saved to files to look at later, but not displayed as figures.
 
-Data = fillGRACE(TnoFill,lwe(1,:),opts);
+Data = graceGapFill(Time_notFilled, lwe(1, :), ...
+   "plotFiguresFlag", plotFiguresFlag, ...
+   "showFiguresFlag", showFiguresFlag, ...
+   "saveFiguresFlag", saveFiguresFlag);
 
+%% Run the algorithm for a shapefile of basins
 
-% % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % 
+% bounds is a structure that contains polyshape objects, one for each
+% basin, created by reading in basin shapefiles and converting the lat-
+% lon values to polyshapes. This loop finds the Grace lat/lon values
+% within each basin+buffer, gap-fills all those points and saves the data
+% one file per basin. see util/pointsinPoly and util/fillGRACE.
+npts = zeros(nbasins, 1);
+for n = 1:nbasins
 
-% This is how I ran the algorithm for a shapefile of basins - 
+   poly = bounds.poly(n).geo;
+   Points = pointsInPoly(lon, lat, poly, buffer);
+   npts(n) = sum(Points.inpoly);
+   Sa = lwe(Points.inpoly,:);
 
-% % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % 
+   % Gap fill at the pixel-scale - VERY SLOW, but necessary to get
+   % error / stdv estimates
 
-% % bounds is a structure that contains polyshape objects, one for each 
-% % basin, created by reading in basin shapefiles and converting the lat-
-% % lon values to polyshapes. This loop finds the Grace lat/lon values 
-% % within each basin+buffer, gap-fills all those points and saves the data 
-% % one file per basin. see util/pointsinPoly and util/fillGRACE. 
-% 
-% for n = 1:nbasins
-%      
-%     poly        = bounds.poly(n).geo;
-%     Points      = pointsInPoly(lon,lat,poly,buffer);
-%     npts(n)     = sum(Points.inpoly);
-%     Sa          = lwe(Points.inpoly,:);
-%     
-%     % NEW, gap fill at the pixel-scale - VERY SLOW, but necessary to get
-%     % error / stdv estimates 
-%     
-%     if gapFill == true
-%         fprintf('working on basin %d\n',n);
-%         
-%         Data = fillGRACE(TnoFill,Sa);
-%         
-%         % note - this saves all the 'cells' meaning the data at all the 
-%         % sub-basin interpolation points
-%         if saveData == true
-%             Data.meta   = bounds.meta(n,:);
-%             sta         = char(bounds.meta.station(n));
-%             save([pathSave 'cells/grace_filled_' sta],'Data');
-%         end
-%         
-%     end
-% end
-% 
-% 
-% 
+   if runGapFillFlag == true
+      fprintf('working on basin %d\n', n);
+      Data = fillGRACE(Time_notFilled, Sa);
+
+      % note - this saves all the 'cells' meaning the data at all the
+      % sub-basin interpolation points
+      if saveDataFlag == true
+         Data.meta = bounds.meta(n,:);
+         sta = char(bounds.meta.station(n));
+         save(fullfile(pathSave, 'cells', ['grace_filled_' sta]), 'Data');
+      end
+   end
+end
