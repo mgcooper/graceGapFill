e87e7477b9edc7f91847ec38dd109866557b8c8c
mgcooper <mguycooper@gmail.com>
2024-07-09 13:43:45 -0700

    style: Organize folders into toolbox structure.

--- toolbox/demo/config.example.m
+++ toolbox/demo/config.example.m
@@ -1,4 +1,4 @@
 function config
-   setenv('GRACE_DATA_PATH', '/path/to/GRACE/data')
+   setenv('GRACE_DATA_PATH', '/path/to/GRACE/data/folder')
    setenv('GRACE_DATA_FILE', 'CSR_GRACE_GRACE-FO_RL06_Mascons_all-corrections_v02.nc')
 end
