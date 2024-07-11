e6741b97aef7d6300e3ca2d050fcc2e568d8e092
mgcooper <mguycooper@gmail.com>
2024-07-10 18:10:20 -0700

    fix: Update with new field names defined in graceGapFill.

--- toolbox/code/plotGapFill.m
+++ toolbox/code/plotGapFill.m
@@ -1,14 +1,22 @@
-function h = plotGapFill(Data,showfigs)
+function varargout = plotGapFill(Data, idx, showfigs)
 
-   time    = datenum(Data.time);
-   X3      = Data.S;
-   X2      = Data.S2;
-   verr1   = Data.Serr(1);
-   verr2   = Data.Serr(2);
-   id      = Data.id;
-   opt_MK  = Data.optMK;
+   if nargin < 2
+      idx = 1;
+   end
+   if nargin < 3
+      showfigs = true;
+   end
 
-   if nargin==1; showfigs = true; end
+   % Extract the data
+   time = datenum(Data.time);
+   S_input = Data.S_input(idx, :);
+   S_part_a = Data.S_part_a(idx, :);
+   S_filled = Data.S_filled(idx, :);
+   S_error_a = Data.S_error(idx, 1);
+   S_error_b = Data.S_error(idx, 2);
+   time_id = Data.id;
+   opt_MK = Data.optimal_MK(idx, :);
+
    if showfigs == false
       h.f = figure('position',[100,300,1028,303],'Visible','off');
    else
@@ -16,13 +24,24 @@ function h = plotGapFill(Data,showfigs)
    end
 
    hold on
-   h.h1 = plot(time,X3,'o-','color',[1,1,1]/2);
-   h.herr1 = errorbar(time(id == 3),X2 (id==3), ones(sum(id==3),1)*verr1, ...
-      'ro','markerfacecolor','r');
-   h.herr2 = errorbar(time(id == 4),X3 (id==4), ones(sum(id==4),1)*verr2, ...
-      'bo','markerfacecolor','b');
-   h.leg = legend('Final series','SSA-filling-a','SSA-filling-b', ...
+   h.errorbar_final_series = plot(time, S_filled, 'o-', 'color', [1,1,1]/2);
+
+   h.errorbar_part_a = errorbar(time(time_id == 3), ...
+      S_part_a(time_id==3), ...
+      ones(sum(time_id==3),1)*S_error_a, ...
+      'ro', 'markerfacecolor', 'r');
+
+   h.errorbar_part_b = errorbar(time(time_id == 4), ...
+      S_filled(time_id==4), ...
+      ones(sum(time_id==4),1)*S_error_b, ...
+      'bo', 'markerfacecolor', 'b');
+
+   h.legend = legend('Final series','SSA-filling-a','SSA-filling-b', ...
       'location','best');
    title(sprintf('Optimal parameter: M=%d, K=%d',opt_MK));
    datetick
+
+   if nargout == 1
+      varargout{1} = h;
+   end
 end
